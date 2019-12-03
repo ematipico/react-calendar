3c2fb828a69c6c168f6e1145193bd2c8f2fb4402
emanuele <my.burning@gmail.com>
2019-12-03 13:23:28 +0000

    tied up CI

---
+++ stories/Datepicker.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { DatePicker } from "../src";
+import "../src/index.css";
+
+export default {
+	title: "Components|Datepicker"
+};
+
+export const DefaultView = () => {
+	return <DatePicker />;
+};
